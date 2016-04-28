dabe76e3d9d592b795762f5eb972e97e11cc6f29
Juliaruth <jhabbe@oxy.edu>
2016-04-27 17:22:14 -0700

    !

--- flask-app.py
+++ flask-app.py
@@ -42,6 +42,7 @@ class Student:
     def __eq__(self, other):
         return self.username == other.username
 
+
 def get_data():
     students = []
     with open('students.csv') as fd:
@@ -53,11 +54,42 @@ def get_data():
 
 @app.route('/directory')
 def view_directory():
-    return 'FIXME'
+    # call the list from get_data directory
+    master_list = get_data()
+    # put list of all students into directory template
+    return render_template('directory.html', students=master_list)
 
 @app.route('/directory/<username>')
 def view_student(username):
-    return 'FIXME'
+    # use list of students
+    master_list = get_data()
+    current_student = None
+    # for every number from zero to the length of the master list...
+    for i in range(0, len(master_list)):
+        # if the property username of the student at that index is the username
+        if master_list[i].username == username:
+            # current student is student at that position in the master list
+            current_student = master_list[i]
+            break
+    # if not first or last student...
+    if i != 0 and i != (len(master_list)-1):
+        # make the previous student the student at the previous index
+        prev_student = master_list[i - 1]
+        # make the next student at the next index
+        next_student = master_list[i + 1]
+    elif i == 0:
+        # make the previous student the last student on the master list
+        prev_student = master_list[(len(master_list))-1]
+        # and the next student the same as normal
+        next_student = master_list[i + 1]
+    # if it's at the last student...
+    elif i == (len(master_list)-1):
+        # previous student same as normal
+        prev_student = master_list[i - 1]
+        # but make the next student the first student on the list
+        next_student = master_list[0]
+    # return template with information defined
+    return render_template('student.html', student=current_student, prev_student=prev_student, next_student=next_student)
 
 # DON'T TOUCH THE CODE BELOW THIS LINE
 
